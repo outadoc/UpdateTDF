762f6b70a37a0cf3ac332e05467be617faddab7f
outadoc <outadoc@gmail.com>
2014-09-20 12:08:19 +0200

    Fix navbar link

--- web/view/navbar.php
+++ web/view/navbar.php
@@ -1,11 +1,11 @@
 <nav class="navbar navbar-default" role="navigation">
 	<div class="container-fluid">
 		<div class="navbar-header">
-			<a class="navbar-brand" href="../includes">Mise à jour TDF</a>
+			<a class="navbar-brand" href="./">Mise à jour TDF</a>
 		</div>
 		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
 			<ul class="nav navbar-nav">
-				<li><a href="../includes">Liste des coureurs</a></li>
+				<li><a href="./">Liste des coureurs</a></li>
 				<li class="dropdown">
 					<a href="#" class="dropdown-toggle" data-toggle="dropdown">Ajouter <span class="caret"></span></a>
 					<ul class="dropdown-menu" role="menu">
