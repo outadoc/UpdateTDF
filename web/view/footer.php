1b1159420631e25f505578e431a5d253ed45b182
outadoc <outadoc@gmail.com>
2014-09-21 10:16:50 +0200

    Ajout d'un pied de page

--- web/view/footer.php
+++ web/view/footer.php
@@ -1,3 +1,8 @@
 </div>
+<div class="footer">
+	<div class="container">
+		<p class="text-muted">&copy; Baptiste CANDELLIER 2014 - DUT Informatique Unicaen 2014/2015 - TD3</p>
+	</div>
+</div>
 </body>
 </html>
