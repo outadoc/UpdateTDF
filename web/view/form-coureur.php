f38b0b8ec382fb84634b2df49f151567b9b2fe1c
outadoc <outadoc@gmail.com>
2014-09-23 16:10:47 +0200

    On récupère les valeurs des champs en cas d'erreur pour la page coureur

--- web/view/form-coureur.php
+++ web/view/form-coureur.php
@@ -6,15 +6,21 @@
 
 	namespace TDF;
 
+
+	/** @var $title string */
+	/** @var $data_prenom string */
+	/** @var $data_nom string */
+	/** @var $data_annee_naissance string */
+	/** @var $data_annee_tdf string */
+	/** @var $data_code_tdf string */
+	/** @var $pays string */
+
 ?>
 <div class="row">
 	<div class="col-md-12">
 		<div class="page-header">
 			<h1>
-				<?php
-					/** @var $title string */
-					echo $title;
-				?>
+				<?php echo $title; ?>
 			</h1>
 		</div>
 	</div>
@@ -26,18 +32,17 @@
 			<?php
 
 				echo FormUtils::getTextField("prenom", "Prénom",
-					(isset($coureur) ? $coureur->PRENOM : ""), true, 20, "Alberto");
+					(isset($coureur) ? $coureur->PRENOM : $data_prenom), true, 20, "Alberto");
 				echo FormUtils::getTextField("nom", "Nom",
-					(isset($coureur) ? $coureur->NOM : ""), true, 30, "CONTADOR");
+					(isset($coureur) ? $coureur->NOM : $data_nom), true, 30, "CONTADOR");
 
 				echo FormUtils::getNumberField("annee_naissance", "Année de naissance", 1800, 2999, 1,
-					(isset($coureur) ? $coureur->ANNEE_NAISSANCE : 1955));
+					(isset($coureur) ? $coureur->ANNEE_NAISSANCE : (($data_annee_naissance != null) ? $data_annee_naissance : 1955)));
 				echo FormUtils::getNumberField("annee_tdf", "Année du 1er TDF", 1903, 2999, 1,
-					(isset($coureur) ? $coureur->ANNEE_TDF : Time::getCurrentYear()));
+					(isset($coureur) ? $coureur->ANNEE_TDF : (($data_annee_tdf != null) ? $data_annee_tdf : Time::getCurrentYear())));
 
-				/** @var $pays string */
 				echo FormUtils::getDropdownList("code_tdf", "Pays", "CODE_TDF", "NOM",
-					$pays, (isset($coureur)) ? $coureur->CODE_TDF : 'FRA');
+					$pays, (isset($coureur)) ? $coureur->CODE_TDF : (($data_code_tdf != null) ? $data_code_tdf : 'FRA'));
 
 				echo '<input type="submit" class="btn btn-default">';
 
