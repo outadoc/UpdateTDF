9f59324617affea368248458f2fadbffaef69da6
outadoc <outadoc@gmail.com>
2014-09-28 15:43:52 +0200

    Suppression du thème bootstrap

--- web/view/header.php
+++ web/view/header.php
@@ -7,7 +7,6 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
 		<link rel="stylesheet" href="view/css/bootstrap.min.css">
-		<link rel="stylesheet" href="view/css/bootstrap-theme.min.css">
 		<link rel="stylesheet" href="view/css/global.css">
 
 		<script type="application/javascript" src="view/js/jquery-2.1.1.min.js"></script>
