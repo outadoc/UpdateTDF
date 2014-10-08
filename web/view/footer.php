3f3f75e466d69d20fc1df1308cb4de9f55c2824a
outadoc <outadoc@gmail.com>
2014-10-08 13:44:19 +0200

    Footer thingies

--- web/view/footer.php
+++ web/view/footer.php
@@ -1,7 +1,7 @@
 </div>
 <div class="footer">
 	<div class="container">
-		<p class="text-muted">&copy; Baptiste CANDELLIER 2014 - DUT Informatique Unicaen 2014/2015 - TD3</p>
+		<p class="text-muted">&copy; Baptiste CANDELLIER 2014 - DUT Informatique 2014/2015 - 2A TD3 - UCBN</p>
 	</div>
 </div>
 </body>
